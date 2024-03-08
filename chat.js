495f497c82186a6ff155c0008928bdd625b5c3cb
Krystal <lyf0921@yahoo.com.hk>
2024-03-08 14:34:33 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,13 +1,12 @@
-const connection = require('./dbConnect');
-const r = require('./requestHandle');
-const util = require('util');
-const myU = require('./utils');
-
+const connection = require("./dbConnect");
+const r = require("./requestHandle");
+const util = require("util");
+const myU = require("./utils");
 
 async function chatList(id) {
   try {
     const query = util.promisify(connection.query).bind(connection);
-    const query1 = `SELECT c.chatId, c.timestamp, c.receivedId AS "sender",
+    const query1 = `SELECT c.chatId, c.createAt, c.receivedId AS "sender",
     CASE
       WHEN c.receivedId LIKE 'e%' THEN ue.eName
       ELSE um.mName
@@ -17,7 +16,7 @@ LEFT JOIN project.user_employer ue ON c.receivedId = ue.eId
 LEFT JOIN project.user_member um ON c.receivedId = um.mId 
 WHERE c.senderId = ? 
 UNION
-SELECT c.chatId, c.timestamp, c.senderId AS "sender",
+SELECT c.chatId, c.createAt, c.senderId AS "sender",
     CASE
       WHEN c.senderId LIKE 'e%' THEN ue.eName
       ELSE um.mName
@@ -26,67 +25,89 @@ FROM project.chat c
 LEFT JOIN project.user_employer ue ON c.senderId = ue.eId 
 LEFT JOIN project.user_member um ON c.senderId = um.mId 
 WHERE c.receivedId = ? 
-ORDER BY chatId`
+ORDER BY chatId`;
 
-    const results = await query(query1, [id, id])
+    const results = await query(query1, [id, id]);
 
-    return r.requestHandle(true, "", 0, results)
+    return r.requestHandle(true, "", 0, results);
   } catch (error) {
     console.log(`error ${error}`);
-    return r.requestHandle(false, error, 1, "")
+    return r.requestHandle(false, error, 1, "");
   }
 }
 
 async function chatMessage(chatId) {
   try {
     const query = util.promisify(connection.query).bind(connection);
-    const results = await query('SELECT * FROM project.chat_content WHERE chatId = ? ', [chatId]);
-    return r.requestHandle(true, "", 0, results)
+    const results = await query(
+      "SELECT * FROM project.chat_content WHERE chatId = ? ",
+      [chatId]
+    );
+    return r.requestHandle(true, "", 0, results);
   } catch (error) {
     console.log(`error ${error}`);
-    return r.requestHandle(false, `${error}`, 1, "")
+    return r.requestHandle(false, `${error}`, 1, "");
   }
 }
-
 
 async function createChat(chat) {
   try {
     const query = util.promisify(connection.query).bind(connection);
-    check = await query('SELECT * FROM project.chat WHERE (senderId = ? AND receivedId = ? )OR (senderId = ? AND receivedId = ?)', [chat.senderId, chat.receivedId, chat.receivedId, chat.senderId]);
+    check = await query(
+      "SELECT * FROM project.chat WHERE (senderId = ? AND receivedId = ? )OR (senderId = ? AND receivedId = ?)",
+      [chat.senderId, chat.receivedId, chat.receivedId, chat.senderId]
+    );
 
     if (check.length > 0) {
-      return r.requestHandle(false, "chat box exist", 1, "")
+      return r.requestHandle(false, "chat box exist", 1, "");
     } else {
-      const results = await query('INSERT INTO project.chat SET ?', chat);
-      console.log('New chat box successfully.');
-      return r.requestHandle(true, 'New chat box added successfully.', 0, chat.chatId)
+      const results = await query("INSERT INTO project.chat SET ?", chat);
+      console.log("New chat box successfully.");
+      return r.requestHandle(
+        true,
+        "New chat box added successfully.",
+        0,
+        chat.chatId
+      );
     }
   } catch (error) {
     console.log(`Error: ${error}`);
-    return r.requestHandle(false, `${error}`, 2, "")
+    return r.requestHandle(false, `${error}`, 2, "");
   }
 }
-
 
 async function createChatMsg(chatMsg) {
   try {
     const query = util.promisify(connection.query).bind(connection);
-    check = await query('SELECT * FROM project.chat WHERE chatId =?', [chatMsg.chatId]);
-    console.log('1')
-    check2 = await query('SELECT * FROM project.chat WHERE senderId = ? OR receivedId = ?', [chatMsg.userId, chatMsg.userId])
+    check = await query("SELECT * FROM project.chat WHERE chatId =?", [
+      chatMsg.chatId,
+    ]);
+    console.log("1");
+    check2 = await query(
+      "SELECT * FROM project.chat WHERE senderId = ? OR receivedId = ?",
+      [chatMsg.userId, chatMsg.userId]
+    );
 
     if (check.length < 1) {
-      return r.requestHandle(false, "chat box not exist", 1, "")
+      return r.requestHandle(false, "chat box not exist", 1, "");
     } else if (check2.length < 1) {
-      return r.requestHandle(false, "chat box error", 2, "")
+      return r.requestHandle(false, "chat box error", 2, "");
     } else {
-      const results = await query('INSERT INTO project.chat_content SET ?', chatMsg);
-      console.log('New chat msg successfully.');
-      return r.requestHandle(true, 'New chat msg added successfully.', 0, chatMsg)
+      const results = await query(
+        "INSERT INTO project.chat_content SET ?",
+        chatMsg
+      );
+      console.log("New chat msg successfully.");
+      return r.requestHandle(
+        true,
+        "New chat msg added successfully.",
+        0,
+        chatMsg
+      );
     }
   } catch (error) {
     console.log(`Error: ${error}`);
-    return r.requestHandle(false, `${error}`, 3, "")
+    return r.requestHandle(false, `${error}`, 3, "");
   }
 }
 
@@ -94,5 +115,5 @@ module.exports = {
   chatList: chatList,
   chatMessage: chatMessage,
   createChat: createChat,
-  createChatMsg: createChatMsg
+  createChatMsg: createChatMsg,
 };
